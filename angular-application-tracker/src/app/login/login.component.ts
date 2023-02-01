c40de44469598cb7e6473c2d981dabd0b222be4c
Alex Murphy <amm1205@rit.edu>
2023-02-01 10:47:17 -0500

    login component login setup

--- angular-application-tracker/src/app/login/login.component.ts
+++ angular-application-tracker/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../login.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private loginService: LoginService,
+  ) { }
 
   ngOnInit(): void {
   }
 
+  login(un: string, pw: string) {
+    this.loginService.postLogIn(un, pw).subscribe();
+  }
 }
