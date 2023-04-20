fc30f6af224c7c6a8a5251b30d71f82844c86ef9
Alex Murphy <amurph0602@icloud.com>
2023-04-20 00:44:11 -0400

    fixed cors 1

---
+++ angular-application-tracker/src/proxy.conf.mjs
@@ -0,0 +1,13 @@
+export default [
+    {
+        context: [
+            '/login', 
+            '/overview',
+            '/user',
+            '/users',
+            '/'
+        ], 
+        target: 'https://vercel.com/amurph2003/application-tracker/GWTcR7ex69aVe8WnAYX15GUvZhLs/',
+        secure: false
+    }
+];
